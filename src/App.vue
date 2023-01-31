4489cd62bbcadb7ff2a882346d49dd71fbe2ab90
Iskander <kubeev2001@gmail.com>
2023-01-31 17:35:36 +0600

    added input

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 
 import TheHeader from "./components/TheHeader.vue";
+import TheInput from "./components/TheInput.vue";
 </script>
 
 <template>
   <div class="wrapper bg-[#141C2F] text-white">
 <TheHeader/>
+    <main class="main container mx-auto">
+    <TheInput/>
+    </main>
   </div>
 </template>
