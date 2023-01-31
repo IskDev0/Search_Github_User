f6f191ec793f3d77bbac8a1fd014676502a531d7
Iskander <kubeev2001@gmail.com>
2023-01-31 17:10:46 +0600

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 
+import TheHeader from "./components/TheHeader.vue";
 </script>
 
 <template>
-
+  <div class="wrapper bg-[#141C2F] text-white">
+<TheHeader/>
+  </div>
 </template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header py-8">
+    <div class="container mx-auto flex justify-between items-center">
+      <a class="stray" href="">devfinder</a>
+      <div class="switch__btn flex items-center gap-4 uppercase stray cursor-pointer">
+        <span class="tracking-widest">Light</span>
+        <img src="../assets/light.svg" alt="">
+      </div>
+    </div>
+
+  </header>
+</template>
+
+<script setup>
+
+</script>
