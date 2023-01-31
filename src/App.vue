94bfe20dd970e25d5b97b3a32bc0c09534cb8696
Iskander <kubeev2001@gmail.com>
2023-01-31 18:11:47 +0600

    added input functionality

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup>
-
 import TheHeader from "./components/TheHeader.vue";
 import TheInput from "./components/TheInput.vue";
+import {ref} from "vue";
+
+let info = ref()
+
+let getData = (data) => {
+  info.value = data.value
+}
+
 </script>
 
 <template>
   <div class="wrapper bg-[#141C2F] text-white">
-<TheHeader/>
+    <TheHeader/>
     <main class="main container mx-auto">
-    <TheInput/>
+      <TheInput @send="getData"/>
+
     </main>
   </div>
 </template>
