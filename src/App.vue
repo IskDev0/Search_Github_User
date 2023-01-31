7c9128009d4495e72c8b4139abbca80f7a31d533
Iskander <kubeev2001@gmail.com>
2023-01-31 19:13:40 +0600

    added user card

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,23 @@
 import TheHeader from "./components/TheHeader.vue";
 import TheInput from "./components/TheInput.vue";
 import {ref} from "vue";
+import UserCard from "./components/UserCard.vue";
 
 let info = ref()
 
 let getData = (data) => {
   info.value = data.value
+  console.log(info.value)
 }
 
 </script>
 
 <template>
-  <div class="wrapper bg-[#141C2F] text-white">
+  <div class="wrapper bg-[#141C2F] text-white h-screen">
     <TheHeader/>
     <main class="main container mx-auto">
       <TheInput @send="getData"/>
-
+      <UserCard v-if="info" :info="info" />
     </main>
   </div>
 </template>
